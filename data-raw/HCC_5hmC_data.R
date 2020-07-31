ef58b21698c84ab44428fae51d8d1708b899f3c7
12379Monty <francois.collin@gmail.com>
2020-07-31 09:43:43 -0700

    Skeleton almost complete

---
+++ data-raw/HCC_5hmC_data.R
@@ -0,0 +1,6 @@
+## code to prepare `HCC_5hmC_data` dataset goes here
+HCC_5hmC_data <- NULL
+ # SEE {Projects}/HCC/R/Scripts/_M1A-readHCCdata.Rmd 
+
+usethis::use_data(HCC_5hmC_data, overwrite = TRUE)
+
