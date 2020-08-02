aba32ba9966a52ce529c1710910947d47c1d9fb1
12379Monty <francois.collin@gmail.com>
2020-08-02 16:17:56 -0700

    debugging data object/datasets issues

--- data-raw/HCC_5hmC_data.R
+++ data-raw/HCC_5hmC_data.R
@@ -2,6 +2,6 @@
 HCC_5hmC_data <- NULL
  # SEE {Projects}/HCC/R/Scripts/_M1A-readHCCdata.Rmd 
 
-#usethis::use_data(HCC_5hmC_data, overwrite = TRUE)
-save(HCC_5hmC_data, file="data/HCC_5hmC_data.rda")
+usethis::use_data(HCC_5hmC_data, overwrite = TRUE)
+#saveRDS(HCC_5hmC_data, file="data/HCC_5hmC_data.rds")
 
