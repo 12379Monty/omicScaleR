01d1ddc6c8d97582684fc3016adeba25870e9329
12379Monty <francois.collin@gmail.com>
2020-08-11 18:04:07 -0700

    clean up skeleton

--- data-raw/HCC_5hmC_data.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `HCC_5hmC_data` dataset goes here
-HCC_5hmC_data <- NULL
- # SEE {Projects}/HCC/R/Scripts/_M1A-readHCCdata.Rmd 
-
-usethis::use_data(HCC_5hmC_data, overwrite = TRUE)
-#saveRDS(HCC_5hmC_data, file="data/HCC_5hmC_data.rds")
-
--- data-raw/dummy.R
+++
@@ -1,9 +0,0 @@
-dummy <- data.table::fread('data-raw/dummy.csv')
-usethis::use_data(dummy, internal=F, overwrite=T)
-#saveRDS(dummy, file='data/dummy.rds')
-
-dummy2 <- data.table::fread('data-raw/dummy.csv')
-usethis::use_data(dummy2, internal=F, overwrite=T)
-#saveRDS(dummy2, file='data/dummy2.rds')
-
-
